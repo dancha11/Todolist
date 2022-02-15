969a04c34c2d4caf2a956ee760e0fc785d312e0f
dancha11 <49191985+dancha11@users.noreply.github.com>
2022-02-15 16:24:42 +0200

    Add files via upload

--- TodoTable.js
+++ TodoTable.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState }  from 'react';
 
-function TodoTable(props) {
+
+function TodoTable() {
+
+  const [todo, setTodo] = useState({desc: '', date: ''});
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
+
+  const deleteItem = (event) => {
+    event.preventDefault();
+    setTodos(todos.filter((item, index) => parseInt(event.target.id) !== index))
+  }
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  } 
   
   
     return (
         <div>
+          <form onSubmit={addTodo}>
+        <input type="date" name="date" value={todo.date} onChange={inputChanged}/>
+        <input type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
+        <input type="submit" value="Add" />
+      </form>
        <table><tbody>
           {
-            props.todos.map((todo, index) => 
+            todos.map((todo, index) => 
               <tr key={index}>
                 <td>{todo.date}</td>
                 <td>{todo.desc}</td>
-                <td><button id={index} onClick={props.deleteItem}>Delete</button></td>
+                <td><button id={index} onClick={deleteItem}>Delete</button></td>
                 </tr>)
           }
         </tbody></table>
